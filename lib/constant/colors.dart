7b28ee2317c4806aa18ae027f1623ae1027d3eaa
Pavin Das <pavindas007@gmail.com>
2024-05-10 10:30:38 +0530

    Working environment created

---
+++ lib/constant/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final mainColor = Colors.purple[300];
+final secColor = Colors.purple[200];
+final triColor = Colors.purple[100];
+const textColor = Colors.white;
---
+++ lib/constant/strings.dart
@@ -0,0 +1,1 @@
+const String appName = "QR Scanner", version = "1.0.0", credit = "@pavin__das";
